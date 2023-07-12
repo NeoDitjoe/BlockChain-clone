36582c06dc08ab31bf6b2669492bbcae5d60938b
Paskay18 <124031153+Paskay18@users.noreply.github.com>
2023-07-12 14:02:54 +0200

    Name changes
    -Renamed Neo's App.jsx to Home.jsx and created another App.jsx because Neo is doing the home page and I am doing the App page

---
+++ blockchain-clone/src/Home.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import Header from './component_home/Header.jsx'
+import Hero from './component_home/Hero.jsx'
+import './App.css'
+
+function Home() {
+
+  return (
+    <>
+    
+      <Header/>
+      <Hero />
+      
+    </>
+  )
+}
+
+export default Home
